11d7a1d392deb0db3e95b2db2616fe5dea7e1440
Hossein Panahi Irdemousa <h.panahi.ird@gmail.com>
2021-07-15 14:01:25 +0430

    navigate to on boarding fragment after 3 seconds

--- app/src/main/java/ir/hpanahi/androidskills/ui/splash/SplashFragment.kt
+++ app/src/main/java/ir/hpanahi/androidskills/ui/splash/SplashFragment.kt
@@ -1,10 +1,12 @@
 package ir.hpanahi.androidskills.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ir.hpanahi.androidskills.R
 
 class SplashFragment : Fragment() {
@@ -14,6 +16,10 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_nav_splash_to_onBoardingFragment)
+        },3000)
+
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
