9a0b7d58dc5b8e29639175b83b2a9e1bf7ee9b84
Hossein Panahi Irdemousa <h.panahi.ird@gmail.com>
2021-07-15 14:00:19 +0430

    create first on boarding screen

---
+++ app/src/main/java/ir/hpanahi/androidskills/ui/onboarding/screens/NameIntroFragment.kt
@@ -0,0 +1,20 @@
+package ir.hpanahi.androidskills.ui.onboarding.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ir.hpanahi.androidskills.R
+
+class NameIntroFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_name_intro, container, false)
+    }
+
+}
