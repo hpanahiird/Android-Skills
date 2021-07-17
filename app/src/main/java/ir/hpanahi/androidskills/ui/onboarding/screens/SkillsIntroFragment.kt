8f59a393eb5aab1b50856a43d018b00a27615aa8
Hossein Panahi Irdemousa <h.panahi.ird@gmail.com>
2021-07-17 22:17:31 +0430

    implement click logic for on boarding screens

--- app/src/main/java/ir/hpanahi/androidskills/ui/onboarding/screens/SkillsIntroFragment.kt
+++ app/src/main/java/ir/hpanahi/androidskills/ui/onboarding/screens/SkillsIntroFragment.kt
@@ -1,11 +1,13 @@
 package ir.hpanahi.androidskills.ui.onboarding.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import ir.hpanahi.androidskills.R
+import kotlinx.android.synthetic.main.fragment_skills_intro.view.*
 
 class SkillsIntroFragment : Fragment() {
 
@@ -14,7 +16,20 @@ class SkillsIntroFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_skills_intro, container, false)
+        val view = inflater.inflate(R.layout.fragment_skills_intro, container, false)
+
+        val viewPager2 =
+            requireActivity().findViewById<ViewPager2>(R.id.viewpager_on_boarding_fragments)
+
+        view.btn_skills_intro_finish.setOnClickListener {
+//            viewPager2.currentItem = 2
+        }
+
+        view.btn_skills_intro_back.setOnClickListener {
+            viewPager2.currentItem = 1
+        }
+
+        return view
     }
 
 }
