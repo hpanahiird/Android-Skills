6c01e527f32488f877561c0f395658bf8339f523
Hossein Panahi Irdemousa <h.panahi.ird@gmail.com>
2021-07-17 21:11:08 +0430

    add view binding feature to project

--- app/src/main/java/ir/hpanahi/androidskills/ui/onboarding/OnBoardingFragment.kt
+++ app/src/main/java/ir/hpanahi/androidskills/ui/onboarding/OnBoardingFragment.kt
@@ -10,6 +10,7 @@ import ir.hpanahi.androidskills.R
 import ir.hpanahi.androidskills.ui.onboarding.screens.NameIntroFragment
 import ir.hpanahi.androidskills.ui.onboarding.screens.ResumeIntroFragment
 import ir.hpanahi.androidskills.ui.onboarding.screens.SkillsIntroFragment
+import kotlinx.android.synthetic.main.fragment_on_boarding.view.*
 
 class OnBoardingFragment : Fragment() {
 
@@ -19,7 +20,7 @@ class OnBoardingFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_on_boarding, container, false)
 
-        val fragments = arrayListOf<Fragment>(
+        val fragments = arrayListOf(
             NameIntroFragment(),
             ResumeIntroFragment(),
             SkillsIntroFragment(),
@@ -27,7 +28,7 @@ class OnBoardingFragment : Fragment() {
 
         val adapter = OnBoardingViewPagerAdapter(fragments, requireActivity().supportFragmentManager, lifecycle)
 
-        view.findViewById<ViewPager2>(R.id.viewpager_on_boarding_fragments).adapter = adapter
+        view.viewpager_on_boarding_fragments.adapter = adapter
 
         return view
     }
