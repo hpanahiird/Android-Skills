02504d9205f48b5df7f178d3f172757fd65477cc
Hossein Panahi Irdemousa <h.panahi.ird@gmail.com>
2021-07-20 15:40:48 +0430

    create dependency Injection AppModule which only injects database.

---
+++ app/src/main/java/ir/hpanahi/androidskills/di/AppModule.kt
@@ -0,0 +1,24 @@
+package ir.hpanahi.androidskills.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import ir.hpanahi.androidskills.C
+import ir.hpanahi.androidskills.data.AppDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext app: Context): AppDatabase {
+        return Room.databaseBuilder(app, AppDatabase::class.java, C.APP_DATABASE_NAME).build()
+    }
+}
