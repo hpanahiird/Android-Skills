19a927960f3b7ec237f433f464aba83709d11413
Hossein Panahi Irdemousa <h.panahi.ird@gmail.com>
2021-07-20 15:39:47 +0430

    create empty AppDatabase class

---
+++ app/src/main/java/ir/hpanahi/androidskills/data/AppDatabase.kt
@@ -0,0 +1,8 @@
+package ir.hpanahi.androidskills.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+}
