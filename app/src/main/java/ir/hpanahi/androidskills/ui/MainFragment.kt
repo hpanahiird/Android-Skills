e1a0937abe88d9b9459821cfdb778a7f873bdca0
Hossein Panahi Irdemousa <h.panahi.ird@gmail.com>
2021-08-18 00:44:21 +0430

    setup bottom navigation with application navigation graph in main fragment

--- app/src/main/java/ir/hpanahi/androidskills/ui/MainFragment.kt
+++ app/src/main/java/ir/hpanahi/androidskills/ui/MainFragment.kt
@@ -5,16 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import ir.hpanahi.androidskills.R
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
 class MainFragment : Fragment() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
 
+        val navHostFragment = childFragmentManager.findFragmentById(
+            R.id.container_app_nav
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Setup the bottom navigation view with navController
+        val bottomNavigationView = view.bottom_nav
+        bottomNavigationView.setupWithNavController(navController)
+
+        return view
+    }
 }
