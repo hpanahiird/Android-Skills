80a8e803a5d2ef39c49d71a7eacd2246ae8c58f6
Hossein Panahi Irdemousa <h.panahi.ird@gmail.com>
2021-08-19 23:05:31 +0430

    create a class for Pexels media api service

---
+++ app/src/main/java/ir/hpanahi/androidskills/api/PexelsService.kt
@@ -0,0 +1,29 @@
+package ir.hpanahi.androidskills.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class PexelsService {
+
+    companion object {
+        private const val BASE_URL = "https://api.pexels.com/"
+
+        fun create(): PexelsService {
+            val logger = HttpLoggingInterceptor().apply { level = Level.BASIC }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(PexelsService::class.java)
+        }
+    }
+}
