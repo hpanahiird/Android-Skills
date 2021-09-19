93b8e95ccbe256f4c509a039bf8ec1eb8466a45c
Hossein Panahi Irdemousa <h.panahi.ird@gmail.com>
2021-09-19 17:06:22 +0430

    assign the adapter to recyclerview in MediaFragment and initiate photo search.

--- app/src/main/java/ir/hpanahi/androidskills/ui/MediaFragment.kt
+++ app/src/main/java/ir/hpanahi/androidskills/ui/MediaFragment.kt
@@ -5,16 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ir.hpanahi.androidskills.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import ir.hpanahi.androidskills.databinding.FragmentMediaBinding
+import ir.hpanahi.androidskills.viewmodels.PhotosViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 
+@AndroidEntryPoint
 class MediaFragment : Fragment() {
+
+    private val adapter = PexelsPhotoAdapter()
+    private val viewModel: PhotosViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_media, container, false)
+        val binding = FragmentMediaBinding.inflate(inflater, container, false)
+        context ?: return binding.root
+
+        binding.photoList.adapter = adapter
+        lifecycleScope.launch {
+            viewModel.searchPhotos("all").collectLatest {
+                adapter.submitData(it)
+            }
+        }
+        return binding.root
     }
 }
