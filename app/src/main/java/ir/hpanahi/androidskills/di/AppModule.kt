d2e560a721f41d0468da80cd22746e68f8aa6ffd
Hossein Panahi Irdemousa <h.panahi.ird@gmail.com>
2021-09-19 16:35:00 +0430

    create a provider for PexelsService in app module

--- app/src/main/java/ir/hpanahi/androidskills/di/AppModule.kt
+++ app/src/main/java/ir/hpanahi/androidskills/di/AppModule.kt
@@ -8,6 +8,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import ir.hpanahi.androidskills.C
+import ir.hpanahi.androidskills.api.PexelsService
 import ir.hpanahi.androidskills.data.AppDatabase
 import javax.inject.Singleton
 
@@ -20,5 +21,11 @@ object AppModule {
     @Provides
     fun provideAppDatabase(@ApplicationContext app: Context): AppDatabase {
         return Room.databaseBuilder(app, AppDatabase::class.java, C.APP_DATABASE_NAME).build()
+    }
+
+    @Singleton
+    @Provides
+    fun providePexelsService(): PexelsService {
+        return PexelsService.create()
     }
 }
